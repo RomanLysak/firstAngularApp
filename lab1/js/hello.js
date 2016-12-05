1822a6552ad4d2817c47caa46cbc2cde1851d9ff
GREY16 <GREY16@ptc.local>
2016-12-05 11:26:33 +0200

    First app

--- lab1/js/hello.js
+++ lab1/js/hello.js
@@ -3,4 +3,8 @@
 angular.module('app', [])
     .controller('HelloController', [function() {
         this.inputField = null;
+        this.greeting = null;
+        this.update = function () {
+            this.greeting =  "Hello Controller " + this.inputField;
+        }
     }]);
